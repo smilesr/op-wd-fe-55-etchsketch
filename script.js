0e7f1f8c4da5c2c6e7298b8345e3ec5c1a2241ea
Robert Smiles Jr <smilesr120@gmail.com>
2016-10-06 14:43:20 -0400

    formatted code

--- script.js
+++ script.js
@@ -1,59 +1,55 @@
 window.onload = function() {
-var response = prompt("What size grid do you want? Ex/ if you want a 4x4 grid, enter: '4'");
-var container = document.createElement("article");
-var startButton = document.createElement("button");
-startButton.innerHTML = "Push to Start Over";
-document.body.appendChild(startButton);
-document.body.appendChild(container);
-var i = 1;
-var gridSize = response * response;
-var setWidth = (100/response) + "%";
-for (i; i <=gridSize; i++){
-  var div = document.createElement("div");
+  var response = prompt("What size grid do you want? Ex/ if you want a 4x4 grid, enter: '4'");
+  var container = document.createElement("article");
+  var startButton = document.createElement("button");
+  startButton.innerHTML = "Push to Start Over";
+  document.body.appendChild(startButton);
+  document.body.appendChild(container);
+  var i = 1;
+  var gridSize = response * response;
+  var setWidth = (100/response) + "%";
+  for (i; i <=gridSize; i++){
+    var div = document.createElement("div");
 
-  div.style.cssText = "background: rgb(0,0,0); color: white; display: flex; justify-content: center; align-items: center; float: left; border: 1px white solid; box-sizing: border-box;"
-  if (response > 24 && response < 49){
-    div.style.height = "75px";
-    div.innerHTML = "<p>SOS</p>";
-  } else if (response >= 49) {
-    div.style.height = "50px";
-    div.innerHTML = "<p>S</p>";
-  } else {
-    div.style.height = "100px";
-    div.innerHTML = "<p>Shiloh</p>";
+    div.style.cssText = "background: rgb(0,0,0); color: white; display: flex; justify-content: center; align-items: center; float: left; border: 1px white solid; box-sizing: border-box;"
+    if (response > 24 && response < 49){
+      div.style.height = "75px";
+      div.innerHTML = "<p>SOS</p>";
+    } else if (response >= 49) {
+      div.style.height = "50px";
+      div.innerHTML = "<p>S</p>";
+    } else {
+      div.style.height = "100px";
+      div.innerHTML = "<p>Shiloh</p>";
+    }
+    div.style.width = setWidth;
+    div.setAttribute("id", "box" + i);
+    container.appendChild(div);
   }
-
-  div.style.width = setWidth;
-  // div.innerHTML = "<p>Shiloh</p>";
-  div.setAttribute("id", "box" + i);
-  container.appendChild(div);
-}
-
-container.addEventListener("mouseover", changeColor, false);
-
-function changeColor(e){
-  if (e.target !== e.Currenttarget){
-    var targetItem = e.target; 
-    var randomNumber = randomNumberGenerator();
-    targetItem.style.background = "rgb(" + randomNumber + ")";
-    targetItem.firstElementChild.style.background = "inherit";
+  container.addEventListener("mouseover", changeColor, false);
+  function changeColor(e){
+    if (e.target !== e.Currenttarget){
+      var targetItem = e.target; 
+      var randomNumber = randomNumberGenerator();
+      targetItem.style.background = "rgb(" + randomNumber + ")";
+      targetItem.firstElementChild.style.background = "inherit";
+    }
+    e.stopPropagation();  
   }
-  e.stopPropagation();  
-}
-function randomNumberGenerator (){
-  var i=0;
-  var arr=[];
-  for (i; i<=2; i++){
-    arr[i] = Math.floor(Math.random() * 255);
+  function randomNumberGenerator (){
+    var i=0;
+    var arr=[];
+    for (i; i<=2; i++){
+      arr[i] = Math.floor(Math.random() * 255);
+    }
+    return arr;
   }
-  return arr;
-}
-startButton.addEventListener("click", function(){
-  var childs = container.childNodes;
-  var kk=0;
-  for (kk; kk<= childs.length; kk++){
-    childs[kk].style.background = "black";
-    childs[kk].firstElementChild.style.background = "black";
-  }
-})
+  startButton.addEventListener("click", function(){
+    var childs = container.childNodes;
+    var kk=0;
+    for (kk; kk<= childs.length; kk++){
+      childs[kk].style.background = "black";
+      childs[kk].firstElementChild.style.background = "black";
+    }
+  })
 }
