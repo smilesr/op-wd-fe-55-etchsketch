8ea38563655fb90a312bf533a7e14130dbf99b95
Robert Smiles Jr <smilesr120@gmail.com>
2016-10-05 23:06:50 -0400

    refactored to minimize references to div.style in the js file

--- script.js
+++ script.js
@@ -6,20 +6,12 @@ document.body.appendChild(container);
 var i = 1;
 for (i; i <=24; i++){
   var div = document.createElement("div");
-  div.style.width = "25%";
-  div.style.height = "100px";
-  div.style.background = "rgb(0,0,0)";
-  div.style.color = "white";
-  div.style.display = "flex";
-  div.style.justifyContent = "center";
-  div.style.alignItems = "center";
+  div.style.cssText = "width: 25%; height: 100px; background: rgb(0,0,0); color: white; display: flex; justify-content: center; align-items: center; float: left; border: 1px white solid; box-sizing: border-box;"
   div.innerHTML = "<p>Shiloh</p>";
-  div.style.float = "left";
-  div.style.border = "1px white solid"
-  div.style.boxSizing = "border-box";
   div.setAttribute("id", "box" + i);
   container.appendChild(div);
 }
+
 container.addEventListener("mouseover", changeColor, false);
 
 function changeColor(e){
